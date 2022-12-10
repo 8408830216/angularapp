5ffde79f29927cc1012988dd61afabae296dfd10
8408830216 <prashantpatil8798@gmail.com>
2022-12-10 15:25:52 +0530

    Updated 10dec
    Updated 10dec

--- src/app/formassignment/formassignment.component.ts
+++ src/app/formassignment/formassignment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Student } from '../models/student';
 
 @Component({
   selector: 'app-formassignment',
@@ -7,14 +8,35 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./formassignment.component.css']
 })
 export class FormassignmentComponent implements OnInit {
-
+  defaultValue:string="UI Developer"
+  student=new Student();
+ submitted:boolean=false;
   constructor() { }
 
   ngOnInit() {
   }
   OnSubmit(form:NgForm){
-    console.log(form);
-    //console.log(form.value);
-      }
+    this.submitted=true;
+    this.student.name=form.value.name;
+    this.student.email=form.value.email;
+    this.student.password=form.value.password;
+    this.student.dateOfBirth=form.value.dateOfBirth;
+    this.student.gender=form.value.gender;
+    this.student.city=form.value.city;
+    this.student.pincode=form.value.pincode
+    this.student.state=form.value.state
+    this.student.country=form.value.country;
+    this.student.course=form.value.course;
+    this.saveStudentData(this.student)
+   }
+   saveStudentData(stu:Student){
+    console.log(stu.name);
+    console.log(stu.email);
+    console.log(stu.password);
+    console.log(stu.dateOfBirth);
+    console.log(stu.gender);
+    console.log(stu.country);
+    console.log(stu.course);
+   }
 
 }
---
+++ src/app/models/student.ts
@@ -0,0 +1,12 @@
+export class Student{
+    name:string;
+    email:string;
+    password:any;
+    dateOfBirth:any;
+    gender:string;
+    city:string;
+    pincode:number;
+    state:string
+    country:string;
+    course:string;
+}
